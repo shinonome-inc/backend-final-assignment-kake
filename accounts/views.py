71e1d4361403997298f9a9e54eb28a1c6e1de930
immunity428 <pega921sus@gmail.com>
2024-08-27 16:25:56 +0900

    サインアップ機能の実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,20 @@
-# from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.urls import reverse_lazy  # エラー1: reverse_lazyをインポート
+from django.views.generic import CreateView
+
+from .forms import SignupForm
+
+
+class SignupView(CreateView):
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("tweets:home")
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        user = authenticate(self.request, username=username, password=password)  # エラー2: passwardをpasswordに修正
+        if user is not None:  # エラー3: 認証成功時のみログインを実行
+            login(self.request, user)
+        return response
