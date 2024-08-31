02600e170423fee25d6a971ecae84ca989ef9642
immunity428 <pega921sus@gmail.com>
2024-08-31 18:53:31 +0900

    TestHomeViewをtweets/test.pyに記述

--- accounts/tests.py
+++ accounts/tests.py
@@ -247,25 +247,3 @@ class TestSignupView(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("確認用パスワードが一致しません。", form.errors["password2"])
         # 「確認用パスワードが一致しません。」などのエラー文は少しでも間違えるとテストでエラーが出るので気を付けること
-
-
-class TestHomeView(TestCase):
-
-    def setUp(self):
-        self.url = reverse("tweets:home")
-
-        # ユーザーを作成
-        self.user = User.objects.create_user(username="tester", password="testpassword")
-
-        # そのユーザーをログイン
-        self.client.login(username="tester", password="testpassword")
-
-    # リクエストの送信
-    def test_success_get(self):
-
-        response = self.client.get(self.url)
-        # self.url = tweets/home
-        # self.client = サーバーに対してリクエストをシミュレート
-
-        self.assertEqual(response.status_code, 200)
-        # assertEqual(A, B) = Aの結果とBが一致すれば成功
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,44 +1,27 @@
-# from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+User = get_user_model()
 
 
-# class TestHomeView(TestCase):
-#     def test_success_get(self):
+class TestHomeView(TestCase):
 
+    def setUp(self):
+        self.url = reverse("tweets:home")
 
-# class TestTweetCreateView(TestCase):
-#     def test_success_get(self):
+        # ユーザーを作成
+        self.user = User.objects.create_user(username="tester", password="testpassword")
 
-#     def test_success_post(self):
+        # そのユーザーをログイン
+        self.client.login(username="tester", password="testpassword")
 
-#     def test_failure_post_with_empty_content(self):
+    # リクエストの送信
+    def test_success_get(self):
 
-#     def test_failure_post_with_too_long_content(self):
+        response = self.client.get(self.url)
+        # self.url = tweets/home
+        # self.client = サーバーに対してリクエストをシミュレート
 
-
-# class TestTweetDetailView(TestCase):
-#     def test_success_get(self):
-
-
-# class TestTweetDeleteView(TestCase):
-#     def test_success_post(self):
-
-#     def test_failure_post_with_not_exist_tweet(self):
-
-#     def test_failure_post_with_incorrect_user(self):
-
-
-# class TestLikeView(TestCase):
-#     def test_success_post(self):
-
-#     def test_failure_post_with_not_exist_tweet(self):
-
-#     def test_failure_post_with_liked_tweet(self):
-
-
-# class TestUnLikeView(TestCase):
-
-#     def test_success_post(self):
-
-#     def test_failure_post_with_not_exist_tweet(self):
-
-#     def test_failure_post_with_unliked_tweet(self):
+        self.assertEqual(response.status_code, 200)
+        # assertEqual(A, B) = Aの結果とBが一致すれば成功
