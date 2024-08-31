ca619680e4b255037081d825f67d81a44df0cd4b
immunity428 <pega921sus@gmail.com>
2024-08-31 09:31:40 +0900

    テストの実装

--- accounts/tests.py
+++ accounts/tests.py
@@ -52,6 +52,27 @@ class TestSignupView(TestCase):
         self.assertIn(SESSION_KEY, self.client.session)
         # self.assertIn(A, B) = AがBに含まれているか
 
+    # フォーム未入力でリクエストの送信
+    def test_failure_post_with_empty_form(self):
+        invalid_data = {
+            "username": "",
+            "email": "",
+            "password1": "",
+            "password2": "",
+        }
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        self.assertIn("このフィールドは必須です。", form.errors["username"])
+        self.assertIn("このフィールドは必須です。", form.errors["email"])
+        self.assertIn("このフィールドは必須です。", form.errors["password1"])
+        self.assertIn("このフィールドは必須です。", form.errors["password2"])
+
     # usernameを設定してないデータでリクエストの送信
     def test_failure_post_with_empty_username(self):
         invalid_data = {
@@ -108,3 +129,143 @@ class TestSignupView(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn("このフィールドは必須です。", form.errors["password1"])
         self.assertIn("このフィールドは必須です。", form.errors["password2"])
+
+    # すでに存在するユーザ名での登録
+    def test_failure_post_with_duplicated_user(self):
+        invalid_data = {
+            "username": "testuser",
+            "email": "test@test.com",
+            "password1": "testpassword",
+            "password2": "testpassword",
+        }
+
+        # テストユーザの作成
+        self.User = get_user_model()
+        self.existing_user = self.User.objects.create_user(username="testuser", password="testpassword")
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(form.is_valid())
+        self.assertIn("同じユーザー名が既に登録済みです。", form.errors["username"])
+
+    # 無効なメールアドレスでのリクエストの送信
+    def test_failure_post_with_invalid_email(self):
+        invalid_data = {
+            "username": "testuser",
+            "email": "testmail",
+            "password1": "testpassword",
+            "password2": "testpassword",
+        }
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        self.assertIn("有効なメールアドレスを入力してください。", form.errors["email"])
+
+    # 短すぎるパスワードでのリクエストの送信
+    def test_failure_post_with_short_password(self):
+        invalid_data = {
+            "username": "testuser",
+            "email": "testmail",
+            "password1": "test",
+            "password2": "test",
+        }
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        # self.assertIn("このパスワードは短すぎます。最低 8 文字以上必要です。", form.errors["password1"])
+        # form.errorsの中に'password1'というキーが存在しない可能性がある
+        self.assertIn("このパスワードは短すぎます。最低 8 文字以上必要です。", form.errors["password2"])
+
+    # ユーザネームと類似したパスワードでのリクエストの送信
+    def test_failure_post_with_password_similar_to_username(self):
+        invalid_data = {
+            "username": "testuser",
+            "email": "testmail",
+            "password1": "testuser",
+            "password2": "testuser",
+        }
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        # self.assertIn("このパスワードは ユーザー名 と似すぎています。", form.errors["password1"])
+        # form.errorsの中に'password1'というキーが存在しない可能性がある
+        self.assertIn("このパスワードは ユーザー名 と似すぎています。", form.errors["password2"])
+
+    # パスワードがすべて数字でのリクエストの送信
+    def test_failure_post_with_only_numbers_password(self):
+        invalid_data = {
+            "username": "testuser",
+            "email": "testmail",
+            "password1": "12345678",
+            "password2": "12345678",
+        }
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        # self.assertIn("このパスワードは数字しか使われていません。", form.errors["password1"])
+        # form.errorsの中に'password1'というキーが存在しない可能性がある
+        self.assertIn("このパスワードは数字しか使われていません。", form.errors["password2"])
+
+    #  password1とpassword2が異なる状態でのリクエストの送信
+    def test_failure_post_with_mismatch_password(self):
+        invalid_data = {
+            "username": "testuser",
+            "email": "testmail",
+            "password1": "testpassword",
+            "password2": "testpassward",
+        }
+
+        response = self.client.post(self.url, invalid_data)
+
+        form = response.context["form"]
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(username=invalid_data["username"]).exists())
+        self.assertFalse(form.is_valid())
+        self.assertIn("確認用パスワードが一致しません。", form.errors["password2"])
+        # 「確認用パスワードが一致しません。」などのエラー文は少しでも間違えるとテストでエラーが出るので気を付けること
+
+
+class TestHomeView(TestCase):
+
+    def setUp(self):
+        self.url = reverse("tweets:home")
+
+        # ユーザーを作成
+        self.user = User.objects.create_user(username="tester", password="testpassword")
+
+        # そのユーザーをログイン
+        self.client.login(username="tester", password="testpassword")
+
+    # リクエストの送信
+    def test_success_get(self):
+
+        response = self.client.get(self.url)
+        # self.url = tweets/home
+        # self.client = サーバーに対してリクエストをシミュレート
+
+        self.assertEqual(response.status_code, 200)
+        # assertEqual(A, B) = Aの結果とBが一致すれば成功
