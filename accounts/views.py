fb0c6c3dbabf98aae95a328e5416bbbf8ee0c1cc
immunity428 <pega921sus@gmail.com>
2024-10-10 11:53:43 +0900

    修正

--- accounts/views.py
+++ accounts/views.py
@@ -25,7 +25,7 @@ class SignupView(CreateView):
 
 class UserProfileView(LoginRequiredMixin, DetailView):
 
-    template_name = "accounts/userprofile.html"
+    template_name = "accounts/user_profile.html"
 
     model = User  # クラス内でグローバル変数を使用
 
